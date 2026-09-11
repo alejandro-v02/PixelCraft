6ede5250b8e0530b7840436c5f9956e468d8b1af
alejandro-v02 <alejandrovitovis@gmail.com>
2026-09-11 10:42:43 -0500

    Highlight selected row with conditional Tailwind classes

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,7 +1,7 @@
 function Button({children, onClick}){
     return(
-        <button onClick={onClick} className="px-4 py-2 bg-amber-700 text-amber-100
-        rounded-lg hover:bg-amber-900 transition-colors duration-400 " >
+        <button onClick={onClick} className="px-4 py-2 bg-amber-700 text-amber-100 
+        rounded-lg hover:bg-amber-900 transition-colors duration-400 cursor-pointer " >
             {children}
         </button>
     );
--- src/modules/Users.jsx
+++ src/modules/Users.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const usersData = [
   { id: 1, name: "Ana Torres", email: "ana@correo.com", role: "Admin" },
   { id: 2, name: "Carlos Ruiz", email: "carlos@correo.com", role: "Instructor" },
@@ -5,27 +7,38 @@ const usersData = [
 ];
 
 function Users() {
+const [selectRow,setSelectRow]=useState(null);
   return (
     <div>
-      <h2>Gestión de Usuarios</h2>
-      <table>
-        <thead>
+      <h2 className=" text-3xl text-center font-bold mt-7">Gestión de Usuarios</h2>
+      <div className="rounded-lg overflow-hidden mt-4 border border-gray-300 " >
+      <table className="w-full  border-collapse rounded-lg " >
+        <thead className="bg-gray-300" >
           <tr>
-            <th>Nombre</th>
-            <th>Email</th>
-            <th>Rol</th>
+            <th className="text-center p-2" >Nombre</th>
+            <th className="text-center p-2" >Email</th>
+            <th className="text-center p-2" >Rol</th>
           </tr>
         </thead>
         <tbody>
-          {usersData.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.role}</td>
+          {usersData.map((user) => {
+            const isSelect=user.id=== selectRow;
+
+            return(
+            <tr key={user.id}
+            onClick={()=>setSelectRow(user.id)}
+            className={`hover:bg-gray-200 transition-colors duration-500 cursor-pointer
+            ${isSelect ? "bg-blue-100" : ""}`} >
+
+              <td className="p-2 text-center  " >{user.name}</td>
+              <td className="p-2 text-center  " >{user.email}</td>
+              <td className="p-2 text-center  " >{user.role}</td>
             </tr>
-          ))}
+            )
+          })}
         </tbody>
       </table>
+      </div>
     </div>
   );
 }
