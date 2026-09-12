4f41fbd0fed54f8c472481793a4a39184fca01cb
alejandro-v02 <alejandrovitovis@gmail.com>
2026-09-12 08:05:24 -0500

    add outline button style

--- src/modules/Buttons.jsx
+++ src/modules/Buttons.jsx
@@ -1,15 +1,25 @@
 function    Buttons(){
     return(
         <div>
-            <h2 className="mt-4 mb-4">Buttons</h2>
+            <div className="flex justify-center items-center text-center" >
+            <h2 className="mt-4 mb-4 font-extrabold text-2xl">Buttons</h2>
+            </div>
             <div>
-                <div className="flex flex-col shadow-lg w-48 h-24 items-center rounded-lg">
-                    <h3 className="mt-2">Solid Button</h3>
+                <div className="flex mt-2 flex-col shadow-lg w-48 h-24 items-center rounded-lg">
+                    <h3 className="mt-2 font-bold">Solid Button</h3>
                     <button
                         className="rounded-full mt-2 bg-blue-600 text-white px-6 py-1.5
                         hover:bg-blue-700 transition-colors duration-500 cursor-pointer">
                         Send
                     </button>
+                </div>
+
+                <div className="flex flex-col mt-2 shadow-lg w-48 h-24 items-center rounded-lg">
+                    <h3>Outline Button</h3>
+                    <button
+                    className="border border-blue-800 text-blue-600 hover:bg-blue-700 hover:text-white cursor-pointer
+                    transition-colors duration-500 rounded-full px-4 py-1.5 "
+                    >Send</button>
                 </div>
             </div>
         </div>
